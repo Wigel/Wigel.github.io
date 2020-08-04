8544bf7990189ec93c3bce827d707d5e79f807fb
Paal <Pal.berglund@gmail.com>
2020-08-05 01:42:23 +0900

    changed domain

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,8 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t "hugo-clarity" # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-
 # Add changes to git.
-git add .
+git add ./docs
 
 # Commit changes.
 msg="rebuilding site $(date)"
